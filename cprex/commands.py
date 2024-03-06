023e344f69e8c88df1645268d660f71e3e588596
Jonas Renault <jonasrenault@gmail.com>
2024-03-06 15:15:38 +0100

    add rel commands

--- cprex/commands.py
+++ cprex/commands.py
@@ -13,6 +13,9 @@ import requests
 from tqdm import tqdm
 
 from cprex.crawler.chemrxiv import download_paper_metadata, download_pdfs_from_dump
+from cprex.ner.chem_ner import get_ner_pipeline
+from cprex.rel.evaluate import evaluate_model
+from cprex.rel.parse_data import parse_label_studio_annotations
 
 PUBMED_BERT_MODEL_URL = "https://ftp.ncbi.nlm.nih.gov/pub/lu/BC7-NLM-Chem-track/model_PubMedBERT_NLMChemBC5CDRBC7Silver.tar.gz"
 GROBID_URL = "https://github.com/kermitt2/grobid/archive/"
@@ -242,6 +245,261 @@ def crawl_chemrxiv(dump_file, save_dir, limit):
     download_paper_metadata(metadata, limit=limit)
 
     download_pdfs_from_dump(metadata, Path(save_dir))
+
+
+@rel.command()
+@click.option(
+    "-c",
+    "--corpus",
+    "corpus_file",
+    help="the annotated corpus file",
+    default="resources/corpus/corpus.json",
+    type=click.Path(file_okay=True),
+)
+@click.option(
+    "-d",
+    "--data-dir",
+    "data_dir",
+    help="output dir for train, dev and test sets",
+    default="resources/rel/data",
+    type=click.Path(dir_okay=True),
+)
+@click.option(
+    "--test/--no-test",
+    help="split data to create a test set",
+    default=True,
+)
+@click.option(
+    "-cv",
+    "--cross-val",
+    "cv",
+    help="use cross validation",
+    default=False,
+    is_flag=True,
+)
+@click.option(
+    "-m",
+    "--mask",
+    "masking",
+    help="obfuscate entity names",
+    default=False,
+    is_flag=True,
+)
+def data(corpus_file: str, data_dir: str, test: bool, cv: bool, masking: bool):
+    click.echo("Loading nlp pipeline...")
+    nlp = get_ner_pipeline(enable_ner_pipelines=False)
+    click.echo(f"Reading annotated corpus {corpus_file}...")
+    parse_label_studio_annotations(
+        Path(corpus_file), Path(data_dir), nlp, test, cv, masking
+    )
+
+
+@rel.command()
+@click.option(
+    "-c",
+    "--config",
+    "config",
+    help="the model config file",
+    default="clee/rel/configs/rel_tok2vec.cfg",
+    type=click.Path(file_okay=True),
+)
+@click.option(
+    "-o",
+    "--output",
+    "output",
+    help="the output directory",
+    default="resources/rel/training",
+    type=click.Path(dir_okay=True),
+)
+@click.option(
+    "-t",
+    "--train",
+    "train_data",
+    help="the train dataset",
+    default="resources/rel/data/train.spacy",
+    type=click.Path(file_okay=True),
+)
+@click.option(
+    "-d",
+    "--dev",
+    "dev_data",
+    help="the dev dataset",
+    default="resources/rel/data/dev.spacy",
+    type=click.Path(file_okay=True),
+)
+def train_tok2vec(config: str, output: str, train_data: str, dev_data: str):
+    command = [
+        "python",
+        "-m",
+        "spacy",
+        "train",
+        config,
+        "--output",
+        output,
+        "--paths.train",
+        train_data,
+        "--paths.dev",
+        dev_data,
+        "-c",
+        "./clee/rel/custom_functions.py",
+    ]
+    subprocess.run(command)
+
+
+@rel.command()
+@click.option(
+    "-c",
+    "--config",
+    "config",
+    help="the model config file",
+    default="clee/rel/configs/rel_trf.cfg",
+    type=click.Path(file_okay=True),
+)
+@click.option(
+    "-o",
+    "--output",
+    "output",
+    help="the output directory",
+    default="resources/rel/training",
+    type=click.Path(dir_okay=True),
+)
+@click.option(
+    "-t",
+    "--train",
+    "train_data",
+    help="the train dataset",
+    default="resources/rel/data/train.spacy",
+    type=click.Path(file_okay=True),
+)
+@click.option(
+    "-d",
+    "--dev",
+    "dev_data",
+    help="the dev dataset",
+    default="resources/rel/data/dev.spacy",
+    type=click.Path(file_okay=True),
+)
+@click.option(
+    "-f",
+    "--force",
+    "force",
+    help="force output directory",
+    default=False,
+    is_flag=True,
+)
+@click.option(
+    "-cv",
+    "--cross-val",
+    "cv",
+    help="run 5 fold cross validation training",
+    default=False,
+    is_flag=True,
+)
+def train_trf(
+    config: str, output: str, train_data: str, dev_data: str, force: bool, cv: bool
+):
+    if cv:
+        # Run 5 training while incrementing output, train and dev files
+        output_dir = get_filename_with_count(Path(output), 0)
+        for fold in range(5):
+            output_dir = increment_directory(str(output_dir), force)
+            train_file = get_filename_with_count(Path(train_data), fold)
+            dev_file = get_filename_with_count(Path(dev_data), fold)
+            click.echo(
+                f"Training fold {fold}: output dir {output_dir}, train file "
+                f"{train_file}, dev file {dev_file}"
+            )
+            command = [
+                "python",
+                "-m",
+                "spacy",
+                "train",
+                config,
+                "--output",
+                str(output_dir),
+                "--paths.train",
+                str(train_file),
+                "--paths.dev",
+                str(dev_file),
+                "-c",
+                "./clee/rel/custom_functions.py",
+                "--gpu-id",
+                "0",
+            ]
+            subprocess.run(command)
+
+    else:
+        output_dir = increment_directory(str(output), force)
+
+        command = [
+            "python",
+            "-m",
+            "spacy",
+            "train",
+            config,
+            "--output",
+            str(output_dir),
+            "--paths.train",
+            train_data,
+            "--paths.dev",
+            dev_data,
+            "-c",
+            "./clee/rel/custom_functions.py",
+            "--gpu-id",
+            "0",
+        ]
+        subprocess.run(command)
+
+
+def increment_directory(dir: str, force: bool) -> Path:
+    output_dir = Path(dir)
+    if (
+        not force
+        and output_dir.exists()
+        and ((output_dir / "model-best").exists() or (output_dir / "model-last").exists())
+    ):
+        count = 0
+        while output_dir.exists():
+            output_dir = get_filename_with_count(Path(dir), count)
+            count += 1
+
+        click.echo(
+            f"Target directory {dir} already exists. Saving results to {output_dir} "
+            "instead. Use -f option to force directory."
+        )
+
+    return output_dir
+
+
+def get_filename_with_count(file: Path, count: int) -> Path:
+    basename = file.stem
+    try:
+        if basename.rindex("_") >= 0 and basename[basename.rindex("_") + 1 :].isdigit():
+            basename = basename[: basename.rindex("_")]
+    except ValueError:
+        pass
+    return file.parent / f"{basename}_{count}{file.suffix}"
+
+
+@rel.command()
+@click.option(
+    "-m",
+    "--model",
+    "model",
+    help="the trained model to evaluate",
+    default="resources/rel/training/model-best",
+    type=click.Path(dir_okay=True),
+)
+@click.option(
+    "-t",
+    "--test-data",
+    "test_data",
+    help="the test dataset",
+    default="resources/rel/data/test.spacy",
+    type=click.Path(file_okay=True),
+)
+def evaluate(model: str, test_data: str):
+    evaluate_model(Path(model), Path(test_data), True)
 
 
 cli = click.CommandCollection(sources=[main, rel])
